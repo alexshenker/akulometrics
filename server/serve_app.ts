8f4f9924d439f1e892f9e48f1d0656b600f94abd
alexshenker <alekseyshenker@gmail.com>
2024-01-08 16:50:00 -0500

    heroku-22 stack does not support serving a static app - applied a recommended solution for the sake of deploying this project on heroku

---
+++ server/serve_app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import path from "path";
+
+//https://medium.com/captainme-ai/deploying-migrating-static-create-react-app-project-to-heroku-22-stack-b19a4255ea7c
+
+//For the sake of the deploying to Heroku, we use this solution
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Check if request is from HTTP and if so, redirect to HTTPS
+app.enable("trust proxy");
+
+app.use((req, res, next) => {
+  req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+});
+
+app.use(express.json());
+
+// Your static site folder name
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// Root Redirects to the build in assets folder
+app.get("/", function (_, res) {
+  res.sendFile(path.join(__dirname, "..", "build"));
+});
+
+// Any Page Redirects to the build in assets folder index.html that will load the react app
+app.get("*", function (_, res) {
+  res.sendFile(path.join(__dirname, "..", "build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Running on port: ", port);
+});
