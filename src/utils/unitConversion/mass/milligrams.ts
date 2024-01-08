5fc9e28a510c1f9306e635f710d8a77768aefcce
alexshenker <alekseyshenker@gmail.com>
2024-01-07 21:31:45 -0500

    fixed mg to kg and ton conversio

--- src/utils/unitConversion/mass/milligrams.ts
+++ src/utils/unitConversion/mass/milligrams.ts
@@ -7,15 +7,15 @@ const milligrams: Converter<Mass> = (units: number) => ({
       case "mg":
         return units;
       case "g":
-        return units * 1000;
+        return units / 1000;
       case "kg":
-        return units * (1000 * 1000);
+        return units / (1000 * 1000);
       case "oz":
         return units / 28349.5;
       case "lb":
         return units / 453592;
       case "t (metric)":
-        return units * (1000 * (1000 * 1000));
+        return units / (1000 * (1000 * 1000));
       default: {
         const x: never = to;
         throw new Error(`Unexpected value ${x}`);
